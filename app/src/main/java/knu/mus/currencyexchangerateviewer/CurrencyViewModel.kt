5fe58b0b0a9369b32eae42ac0e268d5335eb3b56
Elite <sashkofm5@gmail.com>
2025-10-26 18:19:34 +0200

    Simplified VievModel data

--- app/src/main/java/knu/mus/currencyexchangerateviewer/CurrencyViewModel.kt
+++ app/src/main/java/knu/mus/currencyexchangerateviewer/CurrencyViewModel.kt
@@ -2,20 +2,24 @@ package knu.mus.currencyexchangerateviewer
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
-import knu.mus.currencyexchangerateviewer.items.CurrencyItem
+import knu.mus.currencyexchangerateviewer.items.ExchangeRate
 import knu.mus.currencyexchangerateviewer.retrofit.RetrofitCurrency
 
 class CurrencyViewModel {
 
     private val retrofitObject: RetrofitCurrency = RetrofitCurrency()
 
-    private val _currency = MutableLiveData<CurrencyItem>()
-    val currency: LiveData<CurrencyItem>
+    private val _currency = MutableLiveData<List<ExchangeRate>>(listOf())
+    val currency: LiveData<List<ExchangeRate>>
         get() = _currency
 
     fun fetchData(date: String){
         retrofitObject.getCurrencyExchange(date){
-            _currency.value = it
+            if (it?.exchangeRate == null){
+                _currency.value = listOf()
+            }else{
+                _currency.value = it.exchangeRate
+            }
         }
     }
 }
--- app/src/main/java/knu/mus/currencyexchangerateviewer/items/ExchangeRate.kt
+++ app/src/main/java/knu/mus/currencyexchangerateviewer/items/ExchangeRate.kt
@@ -4,16 +4,10 @@ package knu.mus.currencyexchangerateviewer.items
 import com.google.gson.annotations.SerializedName
 
 data class ExchangeRate(
-    @SerializedName("baseCurrency")
-    val baseCurrency: String?,
     @SerializedName("currency")
     val currency: String?,
     @SerializedName("purchaseRate")
     val purchaseRate: Double?,
-    @SerializedName("purchaseRateNB")
-    val purchaseRateNB: Double?,
     @SerializedName("saleRate")
     val saleRate: Double?,
-    @SerializedName("saleRateNB")
-    val saleRateNB: Double?
 )
